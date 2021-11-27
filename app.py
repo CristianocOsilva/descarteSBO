0dbd2932da69ba35b38ed5c028e5cc9644b84bcd
CristianocOsilva <cristianocezar.logistica@gmail.com>
2021-11-27 01:33:30 -0300

    segunda versao

--- app.py
+++ app.py
@@ -9,13 +9,19 @@ db = SQLAlchemy(app)
 class Descarte(db.Model):
     id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(150), nullable=False)
-    localidade = db.Column(db.String(150), nullable=False)
+    endereco = db.Column(db.String(150), nullable=False)
+    numero = db.Column(db.Integer)
+    bairro = db.Column(db.String(100), nullable=False)
+    ecoponto = db.Column(db.String(150), nullable=False)
     item = db.Column(db.String(150), nullable=False)
     quantidade = db.Column(db.Integer)
 
-    def __init__(self, nome, localidade, item, quantidade):
+    def __init__(self, nome, endereco, numero, bairro, ecoponto, item, quantidade):
         self.nome = nome
-        self.localidade = localidade
+        self.endereco = endereco
+        self.numero = numero
+        self.bairro = bairro
+        self.ecoponto = ecoponto
         self.item = item
         self.quantidade = quantidade
 
@@ -32,7 +38,7 @@ def cadastrar():
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
-        descarte = Descarte(request.form['nome'], request.form['localidade'], request.form['item'], request.form['quantidade'])
+        descarte = Descarte(request.form['nome'], request.form['endereco'], request.form['numero'], request.form['bairro'], request.form['ecoponto'], request.form['item'], request.form['quantidade'])
         db.session.add(descarte)
         db.session.commit()
         return redirect(url_for('cadastrar'))
@@ -44,7 +50,10 @@ def edit(id):
     descarte = Descarte.query.get(id)
     if request.method == 'POST':
         descarte.nome = request.form['nome']
-        descarte.localidade = request.form['localidade']
+        descarte.localidade = request.form['endereco']
+        descarte.localidade = request.form['numero']
+        descarte.localidade = request.form['bairro']
+        descarte.localidade = request.form['ecoponto']
         descarte.item = request.form['item']
         descarte.quantidade = request.form['quantidade']
         db.session.commit()
